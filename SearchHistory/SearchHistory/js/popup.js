27b128da80cbcc37cab5f044023f2f197431da45
8LB11L2 <yxcui@isoftstone.com>
2018-07-06 21:46:21 +0900

    1.0.3 修正了删除的逻辑

--- SearchHistory/SearchHistory/js/popup.js
+++ SearchHistory/SearchHistory/js/popup.js
@@ -37,24 +37,20 @@ function createTable(){
 	var today = myselect.options[index].value;
 
 	var searchHistory = bg.searchHistory;
-	//today = today || new Date().toLocaleString().match(/\d{4}\/\d{1,2}\/\d{1,2}/)[0];
-	//searchHistory[today]=searchHistory[today]||{};
 	for(var key in searchHistory[today]){
 		html+="<tr id='"+key+"'><td>"+searchHistory[today][key]+"</td><td>"+key+"</td><td><a href='#'>Delete</a></td></tr>";
 	}
 
 	tbody.innerHTML = html;
 
-	var trs = tbody.getElementsByTagName("tr");
 	var tras = tbody.getElementsByTagName("a");
 	for (var i = tras.length - 1; i >= 0; i--) {
-		tras[i].onclick = (function(i){
+	    tras[i].onclick = (function(trNode){
 			return function(){
-				//var key = trs[i].getElementsByTagName("td")[1].innerHTML;
-				var key = trs[i].id;
-				tbody.removeChild(trs[i]);
+			    var key = trNode.id;
+			    tbody.removeChild(trNode);
 				delete searchHistory[today][key];
 			}
-		})(i);
+	    })(tras[i].parentNode.parentNode);
 	}
 }
