9b288fda3a647845984bd9f5358cd5b13282e8de
8LB11L2 <yxcui@isoftstone.com>
2018-07-06 11:17:24 +0900

    SearchHistrory

---
+++ SearchHistory/SearchHistory/js/background.js
@@ -0,0 +1,55 @@
+var searchHistory = undefined;
+chrome.storage.local.get({"searchHistory":{}}, function(item){
+	searchHistory = item.searchHistory;
+});
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
+	if(typeof(request)=="string"){
+		request = exec(request);
+	}
+	if(request){
+		var today = request.time.match(/\d{4}\/\d{1,2}\/\d{1,2}/)[0];
+		request.time = request.time.match(/\d{1,2}:\d{1,2}:\d{1,2}/)[0];
+		var current = request;
+		searchHistory[today]=searchHistory[today]||{};
+		searchHistory[today][current.key] = current.time;
+
+		chrome.storage.local.set({"searchHistory":searchHistory});
+	}
+});
+
+
+function exec(url){
+	var key = url;
+	if(url.match(/(www.google.)/)){
+		key = url.match(/(\?|&)q=.*?&/); 
+		key = key[0].substring(3).replace("&","");
+	}else if(url.match(/(www.baidu.com)/)){
+		key = url.match(/(\?|&)wd=.*?(&|$)/); 
+		key = key[0].substring(4).replace("&","");
+	}else if(url.match(/(www.bing.com)/)){
+		key = url.match(/(\?|&)q=.*?(&|$)/); 
+		key = key[0].substring(4).replace("&","");
+	}else if(url.match(/(www.so.com)/)){
+		key = url.match(/(\?|&)q=.*?(&|$)/); 
+		key = key[0].substring(4).replace("&","");
+	}else if(url.match(/(www.sogou.com)/)){
+		key = url.match(/(\?|&)query=.*?(&|$)/); 
+		key = key[0].substring(4).replace("&","");
+	}else if(url.match(/(search.yahoo.)/)){
+		key = url.match(/(\?|&)p=.*?(&|$)/); 
+		key = key[0].substring(4).replace("&","");
+	}
+	else{
+		return;
+	}
+
+	key = decodeURIComponent(key);
+	if(!key)return;
+	var time = new Date().toLocaleString();
+
+	var current = {
+		time:time,
+		key:key
+	};
+	return current;
+}
---
+++ SearchHistory/SearchHistory/js/popup.js
@@ -0,0 +1,58 @@
+
+var bg = chrome.extension.getBackgroundPage();
+var tbody =document.getElementById("searchHistory_tbody");
+document.getElementById("refresh").onclick = function () {
+	createTable();
+}
+
+setSelect();
+createTable();
+
+
+function setSelect(){
+	var html = "";
+	var today = today || new Date().toLocaleString().match(/\d{4}\/\d{1,2}\/\d{1,2}/)[0];
+	var searchHistory = bg.searchHistory;
+	searchHistory[today] = searchHistory[today]||{};
+	
+	for(var key in searchHistory){
+		html+="<option value ='"+key+"'>"+key+"</option>";
+	}
+	var mySelect=document.getElementById("selector");
+	mySelect.innerHTML = html;
+	mySelect.onchange = function(){
+		createTable();
+	}
+}
+
+
+
+function createTable(){
+	var html = "";
+	
+	var  myselect=document.getElementById("selector");
+	var index=myselect.selectedIndex ;
+	var today = myselect.options[index].value;
+
+	var searchHistory = bg.searchHistory;
+	//today = today || new Date().toLocaleString().match(/\d{4}\/\d{1,2}\/\d{1,2}/)[0];
+	//searchHistory[today]=searchHistory[today]||{};
+	for(var key in searchHistory[today]){
+		html+="<tr id='"+key+"'><td>"+searchHistory[today][key]+"</td><td>"+key+"</td><td><a href='#'>Delete</a></td></tr>";
+	}
+
+	tbody.innerHTML = html;
+
+	var trs = tbody.getElementsByTagName("tr");
+	var tras = tbody.getElementsByTagName("a");
+	for (var i = tras.length - 1; i >= 0; i--) {
+		tras[i].onclick = (function(i){
+			return function(){
+				//var key = trs[i].getElementsByTagName("td")[1].innerHTML;
+				var key = trs[i].id;
+				tbody.removeChild(trs[i]);
+				delete searchHistory[today][key];
+			}
+		})(i);
+	}
+}
