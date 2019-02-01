241693c9216961090d73c7296db8c903840e9035
8LB11L2 <yxcui@isoftstone.com>
2019-02-01 19:12:44 +0900

    chrome扩展 备忘便签 初版

---
+++ ChromeExpansion/MemoNote/js/background.js
@@ -0,0 +1,92 @@
+﻿function formatData() {
+    var temp = {};
+    for (var date in searchHistory) {
+        var v = date.split('/');
+        var nDate = v[0] + "/" + pf(v[1], 2, '0') + "/" + pf(v[2], 2, '0');
+        temp[nDate] = temp[nDate] || {};
+        for (var key in searchHistory[date]) {
+            var s = searchHistory[date][key].split(':');
+            temp[nDate][key] = pf(s[0], 2, '0') + ":" + pf(s[1], 2, '0') + ":" + pf(s[2], 2, '0');
+        }
+        if (JSON.stringify(temp[nDate]) == "{}") {
+            delete temp[nDate];
+        }
+    }
+    searchHistory = objKeySort(temp);
+    show(RM("finish"));
+}
+
+function objKeySort(obj) {
+    var newkey = Object.keys(obj).sort();
+    var newObj = {};
+    for (var i = 0; i < newkey.length; i++) {
+        if (obj[newkey[i]] instanceof Object) {
+            newObj[newkey[i]] = objKeySort(obj[newkey[i]]);
+        }else{
+            newObj[newkey[i]] = obj[newkey[i]];
+        }
+    }
+    return newObj;
+}
+
+function pf(str, l, c) {
+    return (new Array(l + 1).join(c) + str).slice(-2);
+}
+function getTime() {
+    var date = new Date();
+    return 1900 + date.getYear() + "/" 
+        + pf(date.getMonth() + 1, 2, '0') + "/" 
+        + pf(date.getDate(), 2, '0') + " " 
+        + pf(date.getHours(), 2, '0') + ":" 
+        + pf(date.getMinutes(), 2, '0') + ":" 
+        + pf(date.getSeconds(), 2, '0');
+}
+
+function show(msg) {
+    alert(msg);
+}
+
+function RM(key) {
+    return chrome.i18n.getMessage(key);
+}
+function showVersionUpdate() {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4) {
+            if (xhr.status >= 200 && xhr.status < 300 || xhr.status == 304) {
+                var versionCode = xhr.responseText.match(/versionCode:\[.*\]/)[0];
+                versionCode = versionCode.match(/\[.*\]/)[0];
+                versionCode = versionCode.substring(1, versionCode.length - 1);
+                var reg = new RegExp("versionMsg_" + RM("locale") + ":\\[.*\\]");
+                var versionMsg = reg.exec(xhr.responseText)[0];
+                versionMsg = versionMsg.match(/\[.*\]/)[0];
+                versionMsg = versionMsg.substring(1, versionMsg.length - 1);
+                if (compVersion(RM("version"), versionCode) < 0) {
+                    chrome.notifications.create(null, {
+                        type: 'basic',
+                        iconUrl: 'img/wa.png',
+                        title: RM("versionCode"),
+                        message: versionMsg.split('\\n').join("\n")
+                    });
+                }
+            }
+        }
+    };
+    xhr.open("get", "https://blog.csdn.net/AHcpCuiIan/article/details/81096205", true);
+    xhr.send();
+}
+
+function compVersion(v1,v2) {
+    s1 = v1.split('.');
+    s2 = v2.split('.');
+    for (var i = 0; i < 3; i++) {
+        if (s1[i] == s2[i]) continue;
+        break;
+    }
+    return s1[i] - s2[i];
+}
+
+function mediaCss(document) {
+    var css = screen.width <= 1024 ? "1024" : screen.width <= 1440 ? "1440" : "1960";
+    document.write("<link href='/css/" + css + ".css' rel='stylesheet' type='text/css' />");
+}
---
+++ ChromeExpansion/MemoNote/js/popup.js
@@ -0,0 +1,171 @@
+﻿
+if (navigator.userAgent.indexOf("SE 2.X MetaSr 1.0") > -1) {
+    document.body.style.height = "400px";
+}
+
+//var tasks = bg.tasks || { "list": {}, "over": {} };
+
+$taskList = document.getElementById("task-list");
+$taskOver = document.getElementById("task-over");
+
+var $add = document.getElementById("task-add").getElementsByTagName("a")[0];
+var $pending = document.getElementById("pending").getElementsByTagName("span")[0];
+var $completed = document.getElementById("completed").getElementsByTagName("span")[0];
+
+var bg = chrome.extension.getBackgroundPage();
+$add.innerHTML = bg.RM("add");
+$pending.innerHTML = bg.RM("pending");
+$completed.innerHTML = bg.RM("completed");
+
+document.getElementById("task-add").onclick = function () {
+    addTaskItem();
+}
+
+document.getElementById("input-task").addEventListener("keydown", function () {
+    var evt = window.event || e;
+    if (evt.keyCode == 13) {
+        addTaskItem();
+    }
+}, false);
+
+
+var tasks = undefined;
+chrome.storage.sync.get({ "tasks": { "list": {}, "over": {} } }, function (item) {
+    tasks = item.tasks;
+
+    init();
+
+});
+
+
+
+//初始化
+function init() {
+    //var today = bg.getTime().substring(0, 10);
+
+    //构建【待处理】列表    
+    var html = "";
+    for (var task in tasks.list) {
+        html += taskItemModel(task, 0)
+    }
+    $taskList.innerHTML = html;
+
+    //绑定checkbox事件
+    var task = $taskList.getElementsByClassName("checkbox");
+    for (var i = task.length - 1; i >= 0; i--) {
+        task[i].onclick = (function (trNode) {
+            return function () {
+                var taskValue = trNode.id;
+                $taskList.removeChild(trNode);
+                subTaskItem(taskValue)
+            }
+        })(task[i].parentNode);
+    }
+
+    //构建【已完成】列表
+    var html = "";
+    for (var task in tasks.over) {
+        html += taskItemModel(task, 1);
+    }
+    $taskOver.innerHTML = html;
+
+    //为删除按钮绑定事件
+    var taskdDel = document.getElementById("content").getElementsByClassName("del");
+    for (var i = taskdDel.length - 1; i >= 0; i--) {
+        taskdDel[i].onclick = (function (trNode) {
+            return function () {
+                var taskValue = trNode.id;
+                try{
+                    $taskList.removeChild(trNode);
+                    delete tasks.list[taskValue];
+                }catch(ex){
+
+                }
+                try {
+                    $taskOver.removeChild(trNode);
+                    delete tasks.over[taskValue];
+                } catch (ex) {
+
+                }
+                chrome.storage.sync.set({ "tasks": tasks });
+            }
+        })(taskdDel[i].parentNode);
+    }
+}
+
+
+//添加一个【待处理】
+function addTaskItem() {
+
+    var task = document.getElementById("input-task");
+    var taskValue = task.value;
+
+    $taskList.innerHTML += taskItemModel(taskValue, 0);
+    task.value = "";
+
+    tasks.list[taskValue] = "";
+
+    chrome.storage.sync.set({ "tasks": tasks });
+
+    //checkbox事件
+    var task = $taskList.getElementsByClassName("checkbox");
+    //添加一个item，要为所有的item都重新注册一遍事件
+    for (var i = task.length - 1; i >= 0; i--) {
+        task[i].onclick = (function (trNode) {
+            return function () {
+                var taskValue = trNode.id;
+                $taskList.removeChild(trNode);
+                subTaskItem(taskValue)
+            }
+        })(task[i].parentNode);
+    }
+    //del事件
+    var task = $taskList.getElementsByClassName("del");
+    for (var i = task.length - 1; i >= 0; i--) {
+        task[i].onclick = (function (trNode) {
+            return function () {
+                var taskValue = trNode.id;
+                $taskList.removeChild(trNode);
+                delete tasks.list[taskValue];
+                chrome.storage.sync.set({ "tasks": tasks });
+            }
+        })(task[i].parentNode);
+    }
+}
+
+//添加一个【已完成】
+function subTaskItem(taskValue) {
+
+    $taskOver.innerHTML += taskItemModel(taskValue, 1);
+
+    delete tasks.list[taskValue];
+    tasks.over[taskValue] = "";
+    chrome.storage.sync.set({ "tasks": tasks });
+
+    //del事件
+    var task = $taskOver.getElementsByClassName("del");
+    for (var i = task.length - 1; i >= 0; i--) {
+        task[i].onclick = (function (trNode) {
+            return function () {
+                var taskValue = trNode.id;
+                $taskOver.removeChild(trNode);
+                delete tasks.over[taskValue];
+                chrome.storage.sync.set({ "tasks": tasks });
+            }
+        })(task[i].parentNode);
+    }
+}
+
+//list模块删除 函数绑定
+function bindDel($taskModel,dom) {
+
+}
+
+//任务item模型
+function taskItemModel(taskValue, status) {
+    return "<div class='task-item' id='" + taskValue + "'>"
+                 + "<div class='checkbox checkbox-" + status + "'></div>"
+                 + "<div class='text'>" + taskValue + "</div>"
+                 + "<a href='#' class='del'>" + bg.RM("del") + "</a>"
+                 + "</div>";
+}
